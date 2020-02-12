af77c53ba8e6eeea70726120658be9e56ee780d6
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-02-12 17:37:37 +0100

    Add test for cron/list

--- src/feature/api-v1/routes/v1-cron.e2e.test.js
+++ src/feature/api-v1/routes/v1-cron.e2e.test.js
@@ -23,36 +23,53 @@ describe('v1/cron', () => {
   beforeEach(() => axios.get(`${TEST_SERVER_ROOT}/test/schema-v1/reset`));
   // afterAll(() => axios.get(`${TEST_SERVER_ROOT}/test/schema-v1/reset`));
 
-  it('should upsert a new task', async () => {
-    const r1 = await axios.post(`${TEST_SERVER_ROOT}/api/v1/cron/`, t1);
-    // console.info(r1.data);
-    expect(r1.data.success).toBe(true);
-    expect(r1.data.data.was_created).toBe(true);
+  describe('upsert', () => {
+    it('should upsert a new task', async () => {
+      const r1 = await axios.post(`${TEST_SERVER_ROOT}/api/v1/cron/`, t1);
+      // console.info(r1.data);
+      expect(r1.data.success).toBe(true);
+      expect(r1.data.data.was_created).toBe(true);
+    });
 
-    const r2 = await axios.get(`${TEST_SERVER_ROOT}/api/v1/cron/`);
-    console.log(r2.data);
-  });
-
-  it('should upsert a existing task', async () => {
-    await axios.post(`${TEST_SERVER_ROOT}/api/v1/cron/`, t1);
-    const res = await axios.post(`${TEST_SERVER_ROOT}/api/v1/cron/`, t1);
-    // console.info(res.data);
-    expect(res.data.success).toBe(true);
-    expect(res.data.data.was_created).toBe(false);
+    it('should upsert a existing task', async () => {
+      await axios.post(`${TEST_SERVER_ROOT}/api/v1/cron/`, t1);
+      const res = await axios.post(`${TEST_SERVER_ROOT}/api/v1/cron/`, t1);
+      // console.info(res.data);
+      expect(res.data.success).toBe(true);
+      expect(res.data.data.was_created).toBe(false);
+    });
   });
 
   describe('list', () => {
     const t2 = { ...t1, task_name: 't2' };
     const t3 = { ...t1, task_name: 't3' };
+    const t4 = { ...t1, task_name: 't4', group_name: 'faa' };
+    const t5 = { ...t1, task_name: 't5', group_name: 'faa' };
+    const t6 = { ...t1, task_name: 't6', group_name: 'faa' };
 
     beforeEach(async () => {
       await axios.post(`${TEST_SERVER_ROOT}/api/v1/cron/`, t1);
       await axios.post(`${TEST_SERVER_ROOT}/api/v1/cron/`, t2);
       await axios.post(`${TEST_SERVER_ROOT}/api/v1/cron/`, t3);
+      await axios.post(`${TEST_SERVER_ROOT}/api/v1/cron/`, t4);
+      await axios.post(`${TEST_SERVER_ROOT}/api/v1/cron/`, t5);
+      await axios.post(`${TEST_SERVER_ROOT}/api/v1/cron/`, t6);
     });
 
-    it('should fetch a list of tasks', async () => {
+    it('should fetch all the available tasks', async () => {
       const r1 = await axios.get(`${TEST_SERVER_ROOT}/api/v1/cron/`);
+      expect(r1.data.success).toBe(true);
+      expect(r1.data.data.tasks).toHaveLength(6);
+    });
+
+    it('should fetch a limited amount of tasks', async () => {
+      const r1 = await axios.get(`${TEST_SERVER_ROOT}/api/v1/cron/?limit=1`);
+      expect(r1.data.success).toBe(true);
+      expect(r1.data.data.tasks).toHaveLength(1);
+    });
+
+    it('should fetch tasks by group', async () => {
+      const r1 = await axios.get(`${TEST_SERVER_ROOT}/api/v1/cron/?group=foo`);
       expect(r1.data.success).toBe(true);
       expect(r1.data.data.tasks).toHaveLength(3);
     });
