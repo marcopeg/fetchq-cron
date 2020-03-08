0edfccfe4d4de9e6a0345f96142a2f8984f6ba69
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-03-08 06:20:01 +0000

    Add basic frontend files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Fetchq Cron</h1>
+      <h2>Schedule tasks like a king!</h2>
+    </div>
+  );
+}
